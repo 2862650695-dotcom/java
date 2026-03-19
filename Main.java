c6c359e58e62804272e5337440f3dbaff98c3819
Chen <2862650695@qq.com>
2026-03-19 15:59:39 +0800

    完成Exercise6作业：UserRegistration和Main已放入正确目录

--- Main.java
+++
@@ -1,8 +0,0 @@
-package Excercise6;
-class Main {
-    public static void main(String[] args) {
-        UserRegistration userReg = new UserRegistration();
-        userReg.registration();
-        System.out.println(userReg);
-    }
-}
